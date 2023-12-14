8e58543d9dee83e6a304d2176290c9fb6daf5011
Jay0x13 <dilucrag19@gmail.com>
2023-12-14 16:20:37 +0800

    Update

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from typing import Optional, Tuple, Union
 import customtkinter as ctk
 from CTkMessagebox import CTkMessagebox
-from screenmanager import ScreenManager
 import matplotlib.pyplot as plt
-from header import HeaderSection
 from data import DataScreen
 from result import *
 
@@ -11,26 +9,28 @@ from result import *
 class App(ctk.CTk):
     def __init__(self, fg_color: str | Tuple[str, str] | None = None, **kwargs):
         super().__init__(fg_color, **kwargs)
-        self.geometry('860x600')
+        self.geometry('1000x620')
+        self.title('Skewness and Kurtosis Calculator')
 
         pack_screen = {
             'expand': True,
-            'fill': 'both'
+            'fill': 'both',
+            'side': ctk.LEFT
         }
-
-        self.screenmanager = ScreenManager()
-        self.header = HeaderSection(self, self.screenmanager)
-        self.header.pack(pady=10)
 
         self.data_frame = DataScreen(self)
         self.result_frame = ResultScreen(self)
 
-        self.screenmanager.add_screen('data_screen', self.data_frame, **pack_screen)
-        self.screenmanager.add_screen('result_screen', self.result_frame, **pack_screen)
+        self.data_frame.pack(**pack_screen)
+        self.result_frame.pack(**pack_screen)
 
-        self.screenmanager.set_current('data_screen')
         self.data_frame.result_btn.configure(command=lambda: generate_result(self))
     
+    def resize(self, event):
+        width = event.width // 2
+        self.data_frame.configure(width=width)
+        self.result_frame.configure(width=width)
+
     def close(self):
         if self.result_frame.histfigure:
             plt.close(self.result_frame.histfigure)
